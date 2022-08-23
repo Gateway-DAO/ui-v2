a6c2472b1e783347f46df5c8eed9d8c82824fae3
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-08-23 14:38:39 -0300

    feat: imported utils

---
+++ utils/badge-props.ts
@@ -0,0 +1,11 @@
+import { PartialDeep } from 'type-fest';
+
+import { Gates } from '@/types/hasura.generated';
+
+export const badgeProps = (gate: PartialDeep<Gates>) =>
+  gate?.image
+    ? {
+        src: gate.image,
+        alt: gate.title,
+      }
+    : undefined;
---
+++ utils/objects.ts
@@ -0,0 +1,28 @@
+/** A method that clones an object and remove it's undefined, null or empty values */
+const validateKey = (value: any): boolean => {
+  const isNotUndefined = value !== undefined;
+  const isNotNull = value !== null;
+  const isNotEmpty = value?.length && value.length > 0;
+  return isNotUndefined && isNotNull && isNotEmpty;
+};
+export const clearObject = <T>(obj: T): Partial<T> => {
+  const res = Object.keys(obj).reduce((acc, key) => {
+    const value = obj[key as keyof T];
+    if (validateKey(value)) {
+      acc[key as keyof T] = value;
+    }
+    return acc;
+  }, {} as Partial<T>);
+  return res;
+};
+
+export const getMapValueFromObject = (
+  objectToMap: Record<string, any>,
+  identifier: string,
+  defaultValue: any
+) => {
+  if (objectToMap) {
+    return objectToMap[identifier] || defaultValue;
+  }
+  return defaultValue;
+};
