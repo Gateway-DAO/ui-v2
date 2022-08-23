47afaa8a82a7c29625ad986aaa3e0cdbec067183
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-08-23 11:06:46 -0300

    feat: added graphqlrc

---
+++ .graphqlrc.ts
@@ -0,0 +1,54 @@
+import { IGraphQLConfig } from 'graphql-config';
+
+require('dotenv').config();
+
+const generateConfig = {
+  plugins: [
+    'typescript',
+    'typescript-operations',
+    'typescript-graphql-request',
+    'typescript-resolvers',
+  ],
+  config: {
+    scalars: {
+      _text: 'string',
+    },
+    defaultMapper: 'Partial<{T}>',
+    avoidOptionals: {
+      field: true,
+      inputValue: false,
+      object: true,
+      defaultValue: true,
+    },
+    fetcher: 'graphql-request',
+  },
+};
+
+const config: IGraphQLConfig = {
+  schema: '',
+  extensions: {
+    codegen: {
+      generates: {
+        './services/cyberconnect/types.generated.ts': {
+          ...generateConfig,
+          schema: process.env.NEXT_PUBLIC_CYBERCONNECT_ENDPOINT as string,
+          documents: ['./services/cyberconnect/**/*.{graphql,gql}'],
+        },
+        '.services/hasura/types.generated.ts': {
+          ...generateConfig,
+          schema: {
+            [`${process.env.HASURA_ENDPOINT}`]: {
+              headers: {
+                'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET as string,
+              },
+            },
+          },
+          documents: ['./services/**/*.{graphql,gql}'],
+        }
+      },
+    },
+  },
+}
+
+
+export default config;
