30cb648c62b3fdba3082321f2cc2106c5e814cea
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-08-23 11:19:34 -0300

    feat: next eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import type { AppProps as NextAppProps } from 'next/app'
-import { DashboardApp } from "@/components/app/app"
+import type { AppProps as NextAppProps } from 'next/app';
+
+import { DashboardApp } from '@/components/app/app';
 
 type AppProps = NextAppProps & {
   Component: NextAppProps['Component'] & { auth?: boolean };
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <DashboardApp isAuthPage={Component.auth}>
-    <Component {...pageProps} />
-  </DashboardApp>
+  return (
+    <DashboardApp isAuthPage={Component.auth}>
+      <Component {...pageProps} />
+    </DashboardApp>
+  );
 }
 
-export default MyApp
+export default MyApp;
